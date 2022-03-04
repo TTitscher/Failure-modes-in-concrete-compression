0d65d4992f46c7823ae169d70b019eb0b2dba372
Thomas Titscher <thomas.titscher@bam.de>
2022-03-04 08:52:01 +0100

    Add experimental data

---
+++ code/failure_surface.py
@@ -0,0 +1,15 @@
+"""
+Recreate the typical failure surface plots (in 2D) for a
+given material law.
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+def main():
+    """
+    Yip.
+    """
+
+if __name__ == "__main__":
+    main()
